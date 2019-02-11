0ef054faa9328f4b337969ef6913c6fc024d1673
Julian Lore <julian.lore@mail.mcgill.ca>
2019-02-11 17:27:09 -0500

    Fix run.sh and added ParseCLI

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import ParseCLI
+import qualified Options.Applicative as Op
 
 main :: IO ()
-main = someFunc
+main = do
+  CI cmd inp <- Op.customExecParser (Op.prefs Op.showHelpOnEmpty) cmdParser
+  case cmd of
+    -- Special case, match on file only
+    Codegen -> putStrLn "codegen not yet implemented"
+    _ -> inpToIOStr inp >>= case cmd of
+                              Scan      -> \s -> putStrLn "scan not yet implemented"
+                              Tokens    -> \s -> putStrLn "tokens not yet implemented"
+                              Parse     -> \s -> putStrLn "parse not yet implemented"
+                              Pretty    -> \s -> putStrLn "pretty not yet implemented"
+                              Symbol    -> \s -> putStrLn "symbol not yet implemented"
+                              Typecheck -> \s -> putStrLn "typecheck not yet implemented"
---
+++ src/src/ParseCLI.hs
@@ -0,0 +1,99 @@
+module ParseCLI where
+import Options.Applicative
+
+-- | Inp: type representing input via file or stdin
+-- The user is able to choose which it wants to use via command line arg
+data Inp = FileInp FilePath
+        | StdInp
+
+-- | Cmd: type specifying mode
+data Cmd = Scan
+         | Tokens
+         | Parse
+         | Pretty
+         | Symbol
+         | Typecheck
+         | Codegen
+-- | CmdI: Cmd + Inp
+data CmdI = CI Cmd Inp
+             
+parseFile :: Parser Inp
+parseFile = FileInp <$> strOption
+            ( long "file-path"
+              <> short 'f'
+              <> metavar "FILEPATH"
+              <> help "Read input (source to be evaluated) from file at FILEPATH" )
+
+parseStd :: Parser Inp
+parseStd = flag' StdInp
+           ( long "stdin"
+             <> short 's'
+             <> help "Read input (source to be evaluated) from stdin" )
+  
+parseSource :: Parser Inp
+parseSource = parseFile <|> parseStd
+
+scanParser :: ParserInfo CmdI
+scanParser = info (CI Scan <$> parseSource)
+             (fullDesc
+              <> progDesc "Outputs OK if lexically correct, or an error message."
+              <> header "scan - scans a source file")
+
+tokensParser :: ParserInfo CmdI
+tokensParser = info (CI Tokens <$> parseSource)
+             (fullDesc
+              <> progDesc "Outputs tokens, one per line, until the end of file."
+              <> header "tokens - outputs tokens from the scanner for a source file")
+             
+parseParser :: ParserInfo CmdI
+parseParser = info (CI Parse <$> parseSource)
+             (fullDesc
+              <> progDesc "Outputs OK if syntactically correct, or an error message."
+              <> header "parse - parses a source file")
+
+prettyParser :: ParserInfo CmdI
+prettyParser = info (CI Pretty <$> parseSource)
+               (fullDesc
+               <> progDesc "Outputs pretty printed code from the AST to stdout."
+               <> header "pretty - pretty print a source file")
+
+symbolParser :: ParserInfo CmdI
+symbolParser = info (CI Symbol <$> parseSource)
+               (fullDesc
+               <> progDesc "Outputs the symbol table to stdout or an error if the table is incomplete."
+               <> header "symbol - output the symbol table of a source file")
+
+typecheckParser :: ParserInfo CmdI
+typecheckParser = info (CI Typecheck <$> parseSource)
+                  (fullDesc
+                   <> progDesc "Outputs OK if input type is correct, or an error message."
+                   <> header "typecheck - typechecks a source file")
+  
+codegenParser :: ParserInfo CmdI
+codegenParser = info (CI Codegen <$> parseFile)
+                (fullDesc
+               <> progDesc "Generates equivalent C code in an output file and outputs OK."
+               <> header "codegen - generate C code from a source file")
+
+
+-- Combine all mode parsers into one
+cmdParser :: ParserInfo CmdI
+cmdParser = info
+  ( hsubparser (
+    command "scan" scanParser
+    <> command "tokens" tokensParser
+    <> command "parse" parseParser
+    <> command "pretty" prettyParser
+    <> command "symbol" symbolParser
+    <> command "typecheck" typecheckParser
+    <> command "codegen" codegenParser
+    ) <**> helper)
+  ( fullDesc
+    <> progDesc "Compiler for goLite"
+    <> header "glc - a compiler for goLite" )
+
+inpToIOStr :: Inp -> IO String
+inpToIOStr inp =
+  case inp of
+    FileInp f -> readFile f
+    StdInp -> getContents
