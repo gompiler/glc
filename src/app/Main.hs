fc6129ef363279573c346038c02473908905f422
Allan Wang <me@allanwang.ca>
2019-02-14 22:28:33 -0500

    Format code

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,18 +1,21 @@
 module Main where
 
-import ParseCLI
 import qualified Options.Applicative as Op
+import           ParseCLI
 
 main :: IO ()
 main = do
   CI cmd inp <- Op.customExecParser (Op.prefs Op.showHelpOnEmpty) cmdParser
-  case cmd of
+  case cmd
     -- Special case, match on file only
+        of
     Codegen -> putStrLn "codegen not yet implemented"
-    _ -> inpToIOStr inp >>= case cmd of
-                              Scan      -> \s -> putStrLn "scan not yet implemented"
-                              Tokens    -> \s -> putStrLn "tokens not yet implemented"
-                              Parse     -> \s -> putStrLn "parse not yet implemented"
-                              Pretty    -> \s -> putStrLn "pretty not yet implemented"
-                              Symbol    -> \s -> putStrLn "symbol not yet implemented"
-                              Typecheck -> \s -> putStrLn "typecheck not yet implemented"
+    _ ->
+      inpToIOStr inp >>=
+      case cmd of
+        Scan      -> \s -> putStrLn "scan not yet implemented"
+        Tokens    -> \s -> putStrLn "tokens not yet implemented"
+        Parse     -> \s -> putStrLn "parse not yet implemented"
+        Pretty    -> \s -> putStrLn "pretty not yet implemented"
+        Symbol    -> \s -> putStrLn "symbol not yet implemented"
+        Typecheck -> \s -> putStrLn "typecheck not yet implemented"
