602c86c9606bf56ec26c5a523557eddf3b7af59d
Allan Wang <me@allanwang.ca>
2019-02-16 00:05:44 -0500

    Add all decl

--- src/src/Parser.hs
+++ src/src/Parser.hs
@@ -1,13 +1,114 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Parser where
+
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NonEmpty
+
+-- note that I do not classify blank identifiers as a separate type
+-- because we can easily pattern match it already
+type Identifier = String
 
 type IfStmt = (Expr, Stmt)
 
+type TODO = () -- temp
+
+-- | See https://golang.org/ref/spec#Source_file_organization
+data Program = Program
+  { package   :: String
+  , imports   :: [ImportSpec]
+  -- I'm looking into datakinds. I don't want to declare a new type for topdecl and then link all the constructors again
+  , topLevels :: [TODO]
+  }
+
+-- | Identifier here can either be:
+-- * blank - same name as package
+-- * '.' - imports all packages in folder without qualifier
+-- * '_' - ignored; imported solely for side-effects (initialization)
+-- * some string - qualified name
+data ImportSpec =
+  ImportSpec (Maybe Identifier)
+             String
+
+----------------------------------------------------------------------
+-- Declarations
+-- | See https://golang.org/ref/spec#VarDecl
+-- TODO, I don't really like this format
+data VarDecl =
+  VarDecl Identifier
+          (Either (Type, Maybe Expr) Expr)
+
+data VarDecl'
+  = VarDecl' VarDecl
+  | VarDecl'' [VarDecl]
+
+-- | See https://golang.org/ref/spec#ConstDecl
+data ConstDecl =
+  ConstDecl Identifier
+            (Maybe Type)
+            Expr
+
+data ConstDecl'
+  = ConstDecl' ConstDecl
+  | ConstDecl'' [ConstDecl]
+
+-- | See https://golang.org/ref/spec#TypeDecl
+data TypeDecl
+  = TypeDef Identifier
+            Type
+  | AliasDecl Identifier
+              Type
+
+-- | See https://golang.org/ref/spec#FunctionDecl
+-- If no stmt, func is implemented externally
+data FuncDecl =
+  FuncDecl Identifier
+           Signature
+           (Maybe FuncBody)
+
+type FuncBody = Stmt
+
+data ParameterDecl =
+  ParameterDecl [Identifier]
+                Type
+
+-- Bool refers to variadic type, which may be applied to thee last parameter only
+-- Note that in the specs, it is defined within ParameterDecl, but it makes sense here for the AST
+data Parameters =
+  Parameters [ParameterDecl]
+             Bool
+
+newtype Result =
+  Result (Either Parameters Type)
+
+data Signature =
+  Signature Parameters
+            (Maybe Result)
+
+type Receiver = Parameters
+
+-- | See https://golang.org/ref/spec#Method_declarations
+data MethodDecl =
+  MethodDecl Receiver
+             Signature
+             (Maybe FuncBody)
+
+--data TopDeclaration = ConstDecl | TypeDecl | VarDecl | FuncDecl | MethodDecl
+data Scope
+  = UniverseScope
+  | PackageScope
+  | FuncScope
+  | StmtScope
+
 data Stmt
-  = Block [Stmt]
+  = BlockStmt [Stmt]
   | If IfStmt
        (Maybe IfStmt)
   | While Expr
           Stmt
+  | Blank
 
 -- | Notes on parser todo:
 -- * Integer must parse all valid int types
@@ -19,7 +120,7 @@ data Expr
   | RuneConst Char
   | StringConst StringType
                 String
-  | RawStringConst String
+  | Var Identifier -- todo look at predeclared identifiers? https://golang.org/ref/spec#Predeclared_identifiers
 
 data IntType
   = Decimal
