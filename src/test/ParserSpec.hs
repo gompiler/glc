13124a1caf8ef3d1946e5df3689be015bdcd2b3a
David Lougheed <david.lougheed@gmail.com>
2019-02-27 11:44:20 -0500

    Fix tests

--- src/test/ParserSpec.hs
+++ src/test/ParserSpec.hs
@@ -7,6 +7,9 @@ import CommonTest
 import Parser
 import Data
 
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NonEmpty
+
 {-# ANN module "HLint: ignore Redundant do" #-}
 
 -- | Spec template listing some expected tests; not yet implemented
@@ -16,17 +19,17 @@ spec = do
     mapM_ specWithId expectId
     mapM_ specWithE expectE
 
-specWithId :: (String, Either String [Identifier]) -> SpecWith ()
+specWithId :: (String, Either String (NonEmpty Identifier)) -> SpecWith ()
 specWithId (inp, out) = it (expectStr inp $ show out) $ runAlex inp pId `shouldBe` out
 
-expectId :: [(String, Either String [Identifier])]
+expectId :: [(String, Either String (NonEmpty Identifier))]
 expectId =
-  [ ("testid", Right ["testid"])
-  , ("identttt", Right["identttt"])
-  , ("_", Right["_"])
-  , ("a, b, dddd", Right["a", "b", "dddd"])
-  , ("_, _,_", Right["_", "_", "_"])
-  , ("weirdsp, _   ,aacing", Right["weirdsp", "_", "aacing"])
+  [ ("testid", Right (NonEmpty.fromList ["testid"]))
+  , ("identttt", Right (NonEmpty.fromList ["identttt"]))
+  , ("_", Right (NonEmpty.fromList ["_"]))
+  , ("a, b, dddd", Right (NonEmpty.fromList ["a", "b", "dddd"]))
+  , ("_, _,_", Right (NonEmpty.fromList ["_", "_", "_"]))
+  , ("weirdsp, _   ,aacing", Right (NonEmpty.fromList [ "weirdsp", "_", "aacing"]))
   ]
 
 specWithE :: (String, Either String Expr) -> SpecWith ()
@@ -52,10 +55,10 @@ expectE =
   , ("\"aaaaaaaaax\"", Right $ Lit (StringLit Interpreted "\"aaaaaaaaax\""))
   , ("`aaaaaaaaax`", Right $ Lit (StringLit Raw "`aaaaaaaaax`"))
   ]
-  
+
 intExamples = ["0", "1", "-123", "1234567890", "42", "0600", "0xBadFace", "170141183460469231731687303715884105727"]
 
 floatExamples = [".1234567890", "0.", "72.40", "072.40", "2.71828", "1.e+0", "6.67428e-11", "1E6", ".25", ".12345E+5"]
 
 runeExamples = ['a', 'b', 'c', '\a', '\b', '\f', '\n', '\r', '\t', '\v', '\\', '\'', '\"']
-  
+
