9ec700c645c53e27a3665187790aae1a89ce7bdf
David Lougheed <david.lougheed@gmail.com>
2019-03-01 10:58:51 -0500

    Refactor weeding, add comments, and add for-loop weeding.

--- src/src/Weeding.hs
+++ src/src/Weeding.hs
@@ -1,11 +1,15 @@
 module Weeding where
 
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NonEmpty
+
 import           Data
 import           Data.Maybe  as Maybe
 import           ErrorBundle
 
 type PureConstraint a = a -> Maybe ErrorBundle'
 
+-- | Returns option of either the first element of a list or nothing
 firstOrNothing :: [a] -> Maybe a
 firstOrNothing []    = Nothing
 firstOrNothing (x:_) = Just x
@@ -13,24 +17,48 @@ firstOrNothing (x:_) = Just x
 verifyAll :: PureConstraint a -> [a] -> Maybe ErrorBundle'
 verifyAll constraint items = firstOrNothing $ mapMaybe constraint items
 
+stmtRecursiveVerifyAll :: PureConstraint Stmt -> [Stmt] -> Maybe ErrorBundle'
+stmtRecursiveVerifyAll c stmts = verifyAll (stmtRecursiveVerify c) stmts
+
 -- | Takes a top level statement verifier and applies it to all scopes
 stmtRecursiveVerify :: PureConstraint Stmt -> PureConstraint Stmt
-stmtRecursiveVerify constraint (BlockStmt stmts) = verifyAll (stmtRecursiveVerify constraint) stmts
-stmtRecursiveVerify constraint (If _ s1 s2) = verifyAll (stmtRecursiveVerify constraint) [s1, s2]
-stmtRecursiveVerify constraint (For _ s) = verifyAll (stmtRecursiveVerify constraint) [s]
--- TODO finish
-stmtRecursiveVerify _ _ = undefined
+stmtRecursiveVerify constraint stmt =
+  case stmt of
+    BlockStmt stmts -> stmtRecursiveVerifyAll constraint stmts
+    If _ s1 s2 -> stmtRecursiveVerifyAll constraint [s1, s2]
+    For _ s -> stmtRecursiveVerifyAll constraint [s]
+    -- TODO finish
+    _ -> undefined
 
-switchVerify :: PureConstraint Program
-switchVerify program = firstOrNothing errors
+-- | Extracts block statements from top-level function declarations
+topToStmt :: TopDecl -> Maybe Stmt
+topToStmt (TopFuncDecl (FuncDecl _ _ stmt)) = Just stmt
+topToStmt _                                 = Nothing
+
+-- | Verification rules for specific statements
+stmtVerify :: Stmt -> Maybe ErrorBundle'
+
+stmtVerify (Switch _ _ cases) =
+  -- The [...] pattern matching returns all the examples in the list where the
+  -- pattern was matched (i.e. a default case was found). Effectively a
+  -- pattern-matching filter.
+  case [x | x@(Default _ _) <- cases] of
+    -- This pattern matching checks if the list is of length > 1 AND extracts
+    -- the offset at the same time for error reporting.
+    (_:Default dupOffset _:_) -> Just $ createError dupOffset "Duplicate default found"
+    _ -> Nothing
+
+stmtVerify (For (ForClause _ _ post) _) =
+  case post of
+    ShortDeclare idents _ ->
+      Just $ createError (getOffset $ NonEmpty.toList idents) "For post-statement cannot be declaration"
+    _ -> Nothing
+  where
+    getOffset :: [Identifier] -> Offset
+    getOffset ((Identifier offset _):_) = offset
+
+programVerify :: PureConstraint Program
+programVerify program = firstOrNothing errors
   where
     errors :: [ErrorBundle']
     errors = mapMaybe (stmtRecursiveVerify stmtVerify) (mapMaybe topToStmt $ topLevels program)
-    topToStmt :: TopDecl -> Maybe Stmt
-    topToStmt (TopFuncDecl (FuncDecl _ _ stmt)) = Just stmt
-    topToStmt _                                 = Nothing
-    stmtVerify :: Stmt -> Maybe ErrorBundle'
-    stmtVerify (Switch _ _ cases) =
-      case [x | x@(Default _ _) <- cases] of
-        (_:Default dupOffset _:_) -> Just $ createError dupOffset "Duplicate default found"
-        _ -> Nothing
