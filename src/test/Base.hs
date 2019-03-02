231f382e927b78e06892a3f4a6c92e3e10bb5717
Allan Wang <me@allanwang.ca>
2019-03-02 13:09:06 -0500

    Add prettify testers

--- src/test/Base.hs
+++ src/test/Base.hs
@@ -31,12 +31,14 @@ import Control.Monad (unless)
 import Control.Applicative
 import Data
 import qualified Data.Either as Either
+import Data.List (intercalate)
 import Data.List.NonEmpty (NonEmpty(..), fromList)
 import Data.Text (Text, unpack)
 import ErrorBundle
 import NeatInterpolation
 import Parser (pDec, pE, pEl, pIDecl, pId, pPar, pSig, pStmt, pT, pTDecl)
 import qualified Parser (parse)
+import Prettify
 import Scanner (Alex(..), runAlex)
 import Test.Hspec
 import Test.QuickCheck
@@ -105,6 +107,28 @@ expectAstBase =
            "Invalid ast for:\n\n" ++ toString s ++ "\n\nexpected\n\n" ++ show e ++ "\n\nbut got\n\n" ++ show a)
     (toString . fst)
 
+expectPrettyInvarBase ::
+     (Parsable a, Prettify a, Stringable s) => String -> (String -> Either String a) -> [s] -> SpecWith ()
+expectPrettyInvarBase tag parse =
+  expectBase
+    "pretty invar"
+    (\s ->
+       case multiPass $ toString s of
+         Left err -> expectationFailure err
+         _ -> return ())
+    toString
+    tag
+  where
+    multiPass input = do
+      ast1 <- parse input
+      let pretty1 = prettify ast1
+      ast2 <- parse pretty1
+      let pretty2 = prettify ast2
+      case (ast1 == ast2, pretty1 == pretty2) of
+        (False, _) -> Left $ "AST mismatch: First\n\n" ++ show ast1 ++ "\n\nSecond\n\n" ++ show ast2
+        (_, False) -> Left $ "Prettify mismatch: First\n\n" ++ pretty1 ++ "\n\nSecond\n\n" ++ pretty2
+        _ -> Right ast2
+
 class (Show a, Eq a) =>
       Parsable a
   where
@@ -116,6 +140,7 @@ class (Show a, Eq a) =>
   expectPass :: Stringable s => [s] -> SpecWith ()
   expectFail :: Stringable s => [s] -> SpecWith ()
   expectAst :: Stringable s => [(s, a)] -> SpecWith ()
+  expectPrettyInvar :: Stringable s => [s] -> SpecWith ()
 
 instance Parsable Program where
   tag = "program"
@@ -124,6 +149,7 @@ instance Parsable Program where
   expectPass = expectPassBase (tag @Program) (parse @Program)
   expectFail = expectFailBase (tag @Program) (parse @Program)
   expectAst = expectAstBase (tag @Program)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Program) (parse @Program)
 
 instance Parsable Stmt where
   tag = "stmt"
@@ -132,6 +158,7 @@ instance Parsable Stmt where
   expectPass = expectPassBase (tag @Stmt) (parse @Stmt)
   expectFail = expectFailBase (tag @Stmt) (parse @Stmt)
   expectAst = expectAstBase (tag @Stmt)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Stmt) (parse @Stmt)
 
 instance Parsable TopDecl where
   tag = "topDecl"
@@ -140,6 +167,7 @@ instance Parsable TopDecl where
   expectPass = expectPassBase (tag @TopDecl) (parse @TopDecl)
   expectFail = expectFailBase (tag @TopDecl) (parse @TopDecl)
   expectAst = expectAstBase (tag @TopDecl)
+  expectPrettyInvar = expectPrettyInvarBase (tag @TopDecl) (parse @TopDecl)
 
 instance Parsable Signature where
   tag = "signature"
@@ -148,6 +176,7 @@ instance Parsable Signature where
   expectPass = expectPassBase (tag @Signature) (parse @Signature)
   expectFail = expectFailBase (tag @Signature) (parse @Signature)
   expectAst = expectAstBase (tag @Signature)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Signature) (parse @Signature)
 
 instance Parsable [ParameterDecl] where
   tag = "parameterDecls"
@@ -156,6 +185,10 @@ instance Parsable [ParameterDecl] where
   expectPass = expectPassBase (tag @[ParameterDecl]) (parse @[ParameterDecl])
   expectFail = expectFailBase (tag @[ParameterDecl]) (parse @[ParameterDecl])
   expectAst = expectAstBase (tag @[ParameterDecl])
+  expectPrettyInvar = expectPrettyInvarBase (tag @[ParameterDecl]) (parse @[ParameterDecl])
+
+instance Prettify [ParameterDecl] where
+  prettify' params = prettify' $ Parameters params
 
 instance Parsable Type' where
   tag = "type"
@@ -164,6 +197,7 @@ instance Parsable Type' where
   expectPass = expectPassBase (tag @Type') (parse @Type')
   expectFail = expectFailBase (tag @Type') (parse @Type')
   expectAst = expectAstBase (tag @Type')
+  expectPrettyInvar = expectPrettyInvarBase (tag @Type') (parse @Type')
 
 instance Parsable Decl where
   tag = "decl"
@@ -172,6 +206,7 @@ instance Parsable Decl where
   expectPass = expectPassBase (tag @Decl) (parse @Decl)
   expectFail = expectFailBase (tag @Decl) (parse @Decl)
   expectAst = expectAstBase (tag @Decl)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Decl) (parse @Decl)
 
 instance Parsable [Expr] where
   tag = "exprs"
@@ -180,6 +215,10 @@ instance Parsable [Expr] where
   expectPass = expectPassBase (tag @[Expr]) (parse @[Expr])
   expectFail = expectFailBase (tag @[Expr]) (parse @[Expr])
   expectAst = expectAstBase (tag @[Expr])
+  expectPrettyInvar = expectPrettyInvarBase (tag @[Expr]) (parse @[Expr])
+
+instance Prettify [Expr] where
+  prettify' exprs = [intercalate ", " $ map prettify exprs]
 
 instance Parsable Expr where
   tag = "expr"
@@ -188,6 +227,7 @@ instance Parsable Expr where
   expectPass = expectPassBase (tag @Expr) (parse @Expr)
   expectFail = expectFailBase (tag @Expr) (parse @Expr)
   expectAst = expectAstBase (tag @Expr)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Expr) (parse @Expr)
 
 instance Parsable VarDecl' where
   tag = "varDecl"
@@ -196,14 +236,16 @@ instance Parsable VarDecl' where
   expectPass = expectPassBase (tag @VarDecl') (parse @VarDecl')
   expectFail = expectFailBase (tag @VarDecl') (parse @VarDecl')
   expectAst = expectAstBase (tag @VarDecl')
+  expectPrettyInvar = expectPrettyInvarBase (tag @VarDecl') (parse @VarDecl')
 
 instance Parsable Identifiers where
   tag = "ids"
-  parse' s = fromList <$> scanToP pId s
+  parse' s = fromList . reverse <$> scanToP pId s
   placeholder = Identifier o "temp" :| []
   expectPass = expectPassBase (tag @Identifiers) (parse @Identifiers)
   expectFail = expectFailBase (tag @Identifiers) (parse @Identifiers)
   expectAst = expectAstBase (tag @Identifiers)
+  expectPrettyInvar = expectPrettyInvarBase (tag @Identifiers) (parse @Identifiers)
 
 scanToP :: (Show a, Eq a) => Alex a -> (String -> Either String a)
 scanToP f s = either (\(err, o) -> Left err) Right (runAlex s f)
--- src/test/ParserSpec.hs
+++ src/test/ParserSpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
