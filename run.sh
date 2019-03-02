186488e6efda34f4180281cf6d21b142ef1dc906
Julian Lore <julian.lore@mail.mcgill.ca>
2019-03-02 13:30:33 -0500

    Fixed run script

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ then
     PATH=$csstack:$PATH
     STACK_ROOT=/mnt/local
     export STACK_ROOT
-    STACK_ROOT=/mnt/local stack --allow-different-user exec -- "$DIR"/glc "$1" -f "$2"
+    STACK_ROOT=/mnt/local stack --allow-different-user exec -- "$DIR"/src/glc "$1" -f "$2"
 else
     echo "Newer version of Stack (1.9.3) not found, please you any of the cs-x computers at Trottier or any other computer with Stack 1.9.3, as the default version on other computers is too old."
     exit 1
