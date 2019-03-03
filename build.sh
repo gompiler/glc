f2350c06db60d0094254aee1c34e800f95a3cc2a
Julian Lore <julian.lore@mail.mcgill.ca>
2019-03-02 23:33:15 -0500

    Fixed typo for build and makefile for trottier

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ then
     STACK_ROOT=/mnt/local
     export STACK_ROOT
     
-    make -C src cleantrot
+    make -C src cleanT
     make -C src allT
 else
     echo "Did not find /usr/local/pkgs/haskell/stack-1.9.3-linux-x86_64 and /mnt/local, assuming we are running this locally (not on Trottier), otherwise please use a cs-x.cs.mcgill.ca computer."
