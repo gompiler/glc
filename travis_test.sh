d2ca8ec6bfb7a606e3c19cbf2fe074194fb0d101
Julian Lore <julian.lore@mail.mcgill.ca>
2019-03-04 22:59:45 -0500

    Fixed Travis test script again

--- travis_test.sh
+++ travis_test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-(cd src && stack test --pedantic --coverage)
-
-chmod +x ./test_custom.sh
-./test_custom.sh 
+(cd src && make test)
+ret=$?
+if [[ ret -ne 0 ]]
+then
+    exit 1
+else
+    chmod +x ./test_custom.sh
+    ./test_custom.sh
+fi
