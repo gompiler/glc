7f183a34b1c39b233df95beb5bd1c4c040f875e7
Allan Wang <me@allanwang.ca>
2019-03-11 16:14:42 -0400

    Move formatter to test code and fix tests

--- src/src/Prettify.hs
+++ src/src/Prettify.hs
@@ -3,6 +3,8 @@
 module Prettify
   ( Prettify(..)
   , checkPrettifyInvariance
+  , tabS
+  , tabSize
   ) where
 
 import           Data
@@ -22,18 +24,28 @@ checkPrettifyInvariance input = do
     (_, False) -> Left $ "Prettify mismatch" ++ pretty1 ++ "\n\n" ++ pretty2
     _ -> Right pretty2
 
+tabSize :: Int
+tabSize = 4
+
+tabS :: String
+tabS = replicate tabSize ' '
+
 tab :: [String] -> [String]
-tab = map ("    " ++)
+tab = map (tabS ++)
 
 commaJoin :: Prettify a => [a] -> String
 commaJoin p = intercalate ", " $ map prettify p
 
 -- | Joins last line of first list with first line of last list with a space
 skipNewLine :: [String] -> [String] -> [String]
-[] `skipNewLine` a = a
-a `skipNewLine` [] = a
-[a] `skipNewLine` (b:b') = (a ++ " " ++ b) : b'
-(a:a') `skipNewLine` b = a : a' `skipNewLine` b
+skipNewLine = skipNewLineBase " "
+
+-- | Joins last line of first list with first line of last list with the provided string in between
+skipNewLineBase :: String -> [String] -> [String] -> [String]
+skipNewLineBase _ [] a       = a
+skipNewLineBase _ a []       = a
+skipNewLineBase s [a] (b:b') = (a ++ s ++ b) : b'
+skipNewLineBase s (a:a') b   = a : skipNewLineBase s a' b
 
 -- | Some prettified components span a single line
 -- In that case, we can implement prettify by default
@@ -85,8 +97,10 @@ instance Prettify TypeDef' where
 
 instance Prettify FuncDecl where
   prettify' (FuncDecl ident sig body) =
-    ["func " ++ prettify ident] `skipNewLine`
-    (prettify' sig `skipNewLine` ["{"]) ++
+    skipNewLineBase
+      ""
+      ["func " ++ prettify ident]
+      (prettify' sig `skipNewLine` ["{"]) ++
     tab (prettify' body) ++ ["}"]
 
 instance Prettify ParameterDecl where
@@ -164,8 +178,7 @@ instance Prettify ForClause where
   prettify (ForCond e) = prettify e ++ " "
   prettify (ForClause cs (Just ce) s) =
     prettify cs ++ "; " ++ prettify ce ++ "; " ++ prettify s ++ " "
-  prettify (ForClause cs Nothing s) =
-    prettify cs ++ "; ; " ++ prettify s ++ " "
+  prettify (ForClause cs Nothing s) = prettify cs ++ "; ; " ++ prettify s ++ " "
   prettify' = prettify''
 
 instance Prettify (NonEmpty Expr) where
