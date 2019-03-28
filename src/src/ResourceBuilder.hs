c725ca9579da4c8741f91e1da6aa80b0719cbf6c
Allan Wang <me@allanwang.ca>
2019-03-28 00:52:25 -0400

    Add initial builder rule

--- src/src/ResourceBuilder.hs
+++ src/src/ResourceBuilder.hs
@@ -3,21 +3,19 @@
 module ResourceBuilder where
 
 --import           Base
---import qualified CheckedData  as C
---import           ResourceData
+import qualified CheckedData      as C
+import           Control.Monad.ST
+import qualified ResourceContext  as RC
+import           ResourceData
 
---data ResourceContext = RC
---  { limit   :: Int
---  , allStructs :: [StructType]
-----  , assignVarIndex :: C.ScopedIdent ->
---  }
---
---initContext = RC {limit = 0, allStructs = []}
---
---convertProgram :: C.Program -> Program
---convertProgram = convert initContext
---
---class Converter a b c where
---  convert :: c -> a -> b
---
---instance Converter C.Program Program ResourceContext
+convertProgram :: C.Program -> Program
+convertProgram p =
+  runST $ do
+    rc <- RC.new
+    convert rc p
+
+class Converter a b where
+  convert :: RC.ResourceContext s -> a -> ST s b
+
+instance Converter C.Program Program where
+  convert = undefined
