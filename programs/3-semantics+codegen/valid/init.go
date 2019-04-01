9f47f3beb51d68e84b69804c16feafa54c8ebe4b
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-01 18:05:50 -0400

    Add some test programs

---
+++ programs/3-semantics+codegen/valid/init.go
@@ -0,0 +1,20 @@
+//~1
+//~2
+//~3
+package main
+
+func main() {
+
+}
+
+func init() {
+	println(1)
+}
+
+func init() {
+	println(2)
+}
+
+func init() {
+	println(3)
+}
---
+++ programs/3-semantics+codegen/valid/reserved_java.go
@@ -0,0 +1,221 @@
+//~abstract
+//~assert
+//~boolean
+//~byte
+//~catch
+//~char
+//~class
+//~do
+//~double
+//~enum
+//~extends
+//~final
+//~finally
+//~float
+//~implements
+//~instanceof
+//~int
+//~long
+//~native
+//~new
+//~private
+//~protected
+//~public
+//~short
+//~static
+//~strictfp
+//~super
+//~synchronized
+//~this
+//~throw
+//~throws
+//~transient
+//~try
+//~void
+//~volatile
+//~while
+
+package main
+
+func abstract() {
+	println("abstract")
+}
+
+func assert() {
+	println("assert")
+}
+
+func boolean() {
+	println("boolean")
+}
+
+func byte() {
+	println("byte")
+}
+
+func catch() {
+	println("catch")
+}
+
+func char() {
+	println("char")
+}
+
+func class() {
+	println("class")
+}
+
+func do() {
+	println("do")
+}
+
+func double() {
+	println("double")
+}
+
+func enum() {
+	println("enum")
+}
+
+func extends() {
+	println("extends")
+}
+
+func final() {
+	println("final")
+}
+
+func finally() {
+	println("finally")
+}
+
+func float() {
+	println("float")
+}
+
+func implements() {
+	println("implements")
+}
+
+func instanceof() {
+	println("instanceof")
+}
+
+func int() {
+	println("int")
+}
+
+func long() {
+	println("long")
+}
+
+func native() {
+	println("native")
+}
+
+func new() {
+	println("new")
+}
+
+func private() {
+	println("private")
+}
+
+func protected() {
+	println("protected")
+}
+
+func public() {
+	println("public")
+}
+
+func short() {
+	println("short")
+}
+
+func static() {
+	println("static")
+}
+
+func strictfp() {
+	println("strictfp")
+}
+
+func super() {
+	println("super")
+}
+
+func synchronized() {
+	println("synchronized")
+}
+
+func this() {
+	println("this")
+}
+
+func throw() {
+	println("throw")
+}
+
+func throws() {
+	println("throws")
+}
+
+func transient() {
+	println("transient")
+}
+
+func try() {
+	println("try")
+}
+
+func void() {
+	println("void")
+}
+
+func volatile() {
+	println("volatile")
+}
+
+func while() {
+	println("while")
+}
+
+func main() {
+	abstract()
+	assert()
+	boolean()
+	byte()
+	catch()
+	char()
+	class()
+	do()
+	double()
+	enum()
+	extends()
+	final()
+	finally()
+	float()
+	implements()
+	instanceof()
+	int()
+	long()
+	native()
+	new()
+	private()
+	protected()
+	public()
+	short()
+	static()
+	strictfp()
+	super()
+	synchronized()
+	this()
+	throw()
+	throws()
+	transient()
+	try()
+	void()
+	volatile()
+	while()
+}
