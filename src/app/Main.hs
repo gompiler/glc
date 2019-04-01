440d9abe0c4697f44daee9d4df8c0a99cc0ca6a4
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-01 16:22:17 -0400

    Remove manual exports from CheckedData and move all conversion stuff to CheckedPrettify

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -4,6 +4,7 @@ import           Base
 import qualified Options.Applicative as Op
 import           ParseCLI
 import           Prettify            (checkPrettifyInvariance, prettify)
+import           CheckedPrettify     (prettify)
 import           Scanner             (putExit, putSucc, scanC, scanP)
 import           SymbolTable         (symbol, typecheckP, typecheckGen)
 import           Weeding             (weed)
@@ -26,6 +27,6 @@ main = do
           either putExit (const $ putSucc "OK") . checkPrettifyInvariance
         Symbol -> symbol
         Typecheck -> typecheckP
-        PrettyTypecheck -> either putExit (putStrLn . Prettify.prettify) . typecheckGen
+        PrettyTypecheck -> either putExit (putStrLn . CheckedPrettify.prettify) . typecheckGen
         Codegen ->
           const $ putExit $ createError' "codegen called without filename" -- This should never happen because of case above
--- src/src/Prettify.hs
+++ src/src/Prettify.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications      #-}
 
 module Prettify
