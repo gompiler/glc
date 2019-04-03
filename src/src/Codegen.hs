eda55de2645cc564b4da2f555278bb921cec85e0
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-03 10:26:04 -0400

    Add codegen for methods and fields

--- src/src/Codegen.hs
+++ src/src/Codegen.hs
@@ -14,29 +14,60 @@ class Bytecode a where
 instance Bytecode Class where
   toBC (Class cn fls mts) =
     B.concat
-      [ bstr ".class public "
-      , bstr cn
-      , nl
-      , bstr ".super java/lang/Object"
-      , nl
-      , nl
+      [ bstrM [".class public ", cn, "\n", ".super java/lang/Object", "\n\n"]
       , B.concat $ map toBC fls
       , B.concat $ map toBC mts
       ]
 
 instance Bytecode Field where
-  toBC _ = undefined -- bstr ""
+  toBC (Field acc fn desc)
+    -- .field $(access) $(fieldname) $(signature)
+    -- ex.
+    -- public int thing; ->
+    -- .field public thing I
+   = bstrM [".field ", show acc, " ", fn, " ", desc, "\n"]
 
 instance Bytecode Method where
-  toBC _ = undefined -- bstr ""
+  toBC (Method mn sl ll bod)
+    -- .method public $(methodname)$(signature)
+    -- ex.
+    -- public int main() ->
+    -- .method public main()I
+   =
+    B.concat
+      [ bstrM
+          [ ".method "
+          , "public "
+          , mn
+          , "()V" -- No args and return is void until Method implements signature
+          , "\n"
+          , "\t.limit stack "
+          , show sl
+          , "\n"
+          , "\t.limit locals "
+          , show ll
+          ]
+      , B.concat $ map (tab . toBC) bod
+      ]
+
+instance Bytecode IRItem where
+  toBC _ = undefined
 
 -- | Helper to convert a string to ASCII lazy ByteString
 bstr :: String -> ByteString
 bstr = toLazyByteString . string7
 
+-- | Helper to map bstr over a list of Strings
+bstrM :: [String] -> ByteString
+bstrM sl = B.concat (map bstr sl)
+
 -- | newline
 nl :: ByteString
 nl = bstr "\n"
+
+-- | Prefix bytestring with tab
+tab :: ByteString -> ByteString
+tab s = bstr "\t" `app` s
 
 infixl 4 `app`
 
