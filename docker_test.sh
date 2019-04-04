9811ead65b890c016b9fd5d13e615bb060f308c9
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-04 16:14:26 -0400

    Change python executable name, test CI passing

--- docker_test.sh
+++ docker_test.sh
@@ -3,12 +3,11 @@
 (cd src && stack test --no-install-ghc --system-ghc --coverage --pedantic)
 
 if [[ $? -ne 0 ]]; then
-	exit 1
+    exit 1
 fi
 
-python ./Krakatau/assemble.py main.j
+python3.5 ./Krakatau/assemble.py main.j
 java main
-exit 1
 
 chmod +x ./test_custom.sh
 ./test_custom.sh
