5e13aa30e221f61855c9f79b0619fd8f5cb07bea
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-07 04:32:15 -0400

    Update blank declaration test and add overshadow declaration test

--- programs/3-semantics+codegen/valid/declblank.go
+++ programs/3-semantics+codegen/valid/declblank.go
@@ -1,7 +1,19 @@
 //~11
 //~39
 //~22
+//~tttt
+//~4
 package main
+
+type l struct {
+	_ int
+}
+
+type l2 struct {
+	_ int
+	a int
+	_ int
+}
 
 func _() {
 	println(31)
@@ -15,6 +27,21 @@ func _(_, _, _ int) {
 	println(33)
 }
 
+func test(_ int, _ int) {
+	println("tttt")
+}
+
+func retblank() l {
+	var r l
+	return r
+}
+
+func retl2(n int) l2 {
+	var r l2
+	r.a = n
+	return r
+}
+
 func p(n int) int{
 	println(n)
 	return n
@@ -23,4 +50,8 @@ func p(n int) int{
 func main() {
 	_, a, _ := p(11), 3, p(39)
 	_ = p(22)
+	_ = a
+	test(5, 3)
+	_ = retblank()
+	println(retl2(4).a)
 }
