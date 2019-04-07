fba89fb9ca65f6fe8b0ad5ceed7aa7555226a3a8
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-07 06:10:04 -0400

    Update return test to include strings

--- programs/3-semantics+codegen/valid/return.go
+++ programs/3-semantics+codegen/valid/return.go
@@ -1,4 +1,4 @@
-//~0 32 0
+//~0 32 0 hello
 package main
 
 func reta(r [9]int)[9]int{
@@ -13,14 +13,21 @@ func rets(r struct {a int; })struct {a int;} {
 	return r
 }
 
+func retstr(r string) string {
+	return r
+}
+
 func main() {
 	var arr [9]int
 	var sl []int
 	sl = append(sl, 11)
 	var s struct {a int;}
-	a, b, c := reta(arr), retsl(sl), rets(s)
+	var str = "hello"
+	a, b, c, d := reta(arr), retsl(sl), rets(s), retstr(str)
 	a[0] = 44
 	b[0] = 32
 	c.a = 11
-	println(arr[0], sl[0], s.a)
+	d = "eee"
+	_ = d
+	println(arr[0], sl[0], s.a, str)
 }
