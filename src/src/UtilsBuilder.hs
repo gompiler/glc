de6d93b7a4625cca3caa164d16581dfd93e35dea
Allan Wang <me@allanwang.ca>
2019-04-09 18:44:45 -0400

    Add primitive template

--- src/src/UtilsBuilder.hs
+++ src/src/UtilsBuilder.hs
@@ -23,7 +23,7 @@ data Category = Category
   }
 
 generateUtils :: [Category] -> [String]
-generateUtils categories = map unpack $ generateUtils' =<< categories
+generateUtils categories = map unpack $ (generateUtils' =<< categories) ++ [primitiveArrayTemplate "int"]
 
 generateUtils' :: Category -> [Text]
 generateUtils' Category {baseType = Custom baseClass, arrayDepth, sliceDepth} =
@@ -75,3 +75,99 @@ generateArray baseClass depth =
     arrayClass :: Int -> String
     arrayClass 0 = baseClass
     arrayClass i = "GlcArray_" ++ baseClass ++ "_" ++ show i
+
+primitiveArrayTemplate :: String -> Text
+primitiveArrayTemplate className = primitiveArrayTemplate' (pack className)
+  where
+    primitiveArrayTemplate' :: Text -> Text
+    primitiveArrayTemplate' baseClass =
+      [text|
+      package glcutils;
+
+      import java.util.Arrays;
+
+      public class Glc${baseClass}Array {
+          int length;
+          ${baseClass}[] array;
+
+          public Glc${baseClass}Array(int length) {
+              this(length, null);
+          }
+
+          Glc${baseClass}Array(int length, ${baseClass}[] array) {
+              this.length = length;
+              this.array = array;
+          }
+
+          /**
+           * Ensures that array is nonnull
+           */
+          final void init() {
+              if (array == null) {
+                  array = new ${baseClass}[length];
+              }
+          }
+
+          /**
+           * Return nonnull struct if index is within bounds
+           */
+          public final ${baseClass} get(int i) {
+              init();
+              return array[i];
+          }
+
+          /**
+           * Set new struct value at specified index if it is within bounds
+           */
+          public final void set(int i, ${baseClass} t) {
+              init();
+              array[i] = t;
+          }
+
+          /**
+           * Gets the length of the array, representative of the number of elements
+           * stored
+           */
+          public final int length() {
+              return length;
+          }
+
+          /**
+           * Gets the capacity of the array, representative of the number of elements
+           * that can be stored
+           */
+          public final int capacity() {
+              return array == null ? 0 : array.length;
+          }
+
+          @Override
+          public boolean equals(Object obj) {
+              if (this == obj) {
+                  return true;
+              }
+              if (obj == null || obj.getClass() != getClass()) {
+                  return false;
+              }
+              Glc${baseClass}Array other = (Glc${baseClass}Array) obj;
+              if (length != other.length) {
+                  return false;
+              }
+              if (array == other.array) {
+                  return true;
+              }
+              init();
+              other.init();
+              return Arrays.equals(array, other.array);
+          }
+
+          @Override
+          public int hashCode() {
+              return Arrays.hashCode(array);
+          }
+
+          @Override
+          public String toString() {
+              return Arrays.toString(array);
+          }
+      }
+      |]
