c597471ed73c34619e01834025600cba07140cb4
Allan Wang <me@allanwang.ca>
2019-04-09 19:54:09 -0400

    Test multi slice set

--- glcgutils/src/test/java/glcutils/GlcMutiSliceTest.java
+++ glcgutils/src/test/java/glcutils/GlcMutiSliceTest.java
@@ -2,7 +2,7 @@ package glcutils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class GlcMutiSliceTest {
 
@@ -11,6 +11,8 @@ public class GlcMutiSliceTest {
         GlcSlice$String$2 a = new GlcSlice$String$2();
         a = a.append(null);
         assertNotNull(a.get(0));
+        a.set(0, a.get(0).append("Hello"));
+        assertEquals("Hello", a.get(0).get(0));
     }
 
     /*
