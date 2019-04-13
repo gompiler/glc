d10443093413bd388cc9aab0a7735407d4620e93
Allan Wang <me@allanwang.ca>
2019-04-12 23:05:44 -0400

    Add print tests

---
+++ programs/3-semantics+codegen/valid/prints.go
@@ -0,0 +1,25 @@
+//~
+//~0
+//~1
+//~-1
+//~1
+//~-3.300000e+000
+//~+3.300000e+000
+//~hello	world
+//~hello\tworld
+//~a
+package main
+
+func main() {
+	var a string
+	println(a) // null
+	println(0)
+	println(1)
+	println(-1)
+	println(-(-1))
+	println(-3.3)
+	println(3.3)
+	println("hello\tworld")
+	println(`hello\tworld`)
+	println('a')
+}
