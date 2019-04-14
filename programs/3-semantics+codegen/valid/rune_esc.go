c2b3673006068dc13a4fc68fbd5a8b9cda3b3fbd
Julian Lore <julian.lore@mail.mcgill.ca>
2019-04-14 16:05:11 -0400

    Add rune esc test program

---
+++ programs/3-semantics+codegen/valid/rune_esc.go
@@ -0,0 +1,22 @@
+//~
+//~
+//~
+//~
+//~
+//~
+//~	
+//~
+//~"
+package main
+
+func main() {
+	println(string('\a'))
+	println(string('\b'))
+	println(string('\f'))
+	println(string('\n'))
+	println(string('\r'))
+	println(string('\t'))
+	println(string('\v'))
+	println(string('"'))
+}
+
